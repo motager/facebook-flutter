17e237867c954f0fc66c67414da29da12ab40aaa
motager <maltager658@gmail.com>
2023-09-13 12:33:27 +0300

    new change
    Signed-off-by: motager <maltager658@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@
 import 'package:flutter/material.dart';
 // ignore: unnecessary_import
 import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -87,39 +88,18 @@ class FacebookApp extends StatelessWidget {
               child: Title(
                 color: Colors.green,
                 child: const Text(
-                  "bmw",
+                  "Facebook",
                   style: TextStyle(
                     fontFamily: "myfont",
                     fontSize: 30.0,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ),
-            ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.elliptical(100, 50)),
-              child: Column(
-                children: [
-                 
-                  // Image.network(
-                  //   "https://images.unsplash.com/flagged/photo-1553505192-acca7d4509be?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1790&q=80",
-                  //   loadingBuilder: (context, child, progress) {
-                  //     return progress == null
-                  //         ? child
-                  //         : Container(child: CircularProgressIndicator(),
-                  //         padding: EdgeInsets.fromLTRB(166, 122, 0, 0),
-                  //         );
-
-                  //   },
-                  // ),
                 
-             CircleAvatar(
-           backgroundImage: AssetImage("assets/images/bm1.jpg"),
-           radius: 200,
-             ), 
-                ],
               ),
             ),
+      SvgPicture.asset("assets/img/facebook.svg"),
+      
           ],
         ),
       ),
